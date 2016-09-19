e26f9655943e192064c1175861ce42d503630051
Geoff W <worleyg@cwu.edu>
2016-09-19 02:38:51 -0700

    Fiddled with response and ID appends to result div
    Need to figure out how to get id value out of successCB and into main
    jQuery function.

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
-var searchResult = [];
-var actorId = 0;
-
 function successCB(data) {
 		var dataResult = JSON.parse(data);
 		console.log(dataResult);
 		
-		alert(dataResult.results[0]["id"]);
-		actorId = dataResult.results[0]["id"]
-		
-		$("#resultPanel").append(actorId);
+		actorId = dataResult.results[0]["id"];
+		$("#actor").append(actorId);
+		return actorId;
 };
 
 function errorCB(data) {
             console.log("Error callback: " + data);
-    };
-
-
+};
 
 $(document).ready(function(){
 	$("#resultPanel").hide();
 
 	//search
 	$("#submit").click(function(){
-		var input = $("#userInput").val();
-		//var result = theMovieDb.search.getPerson({"query": input}, successCB, errorCB);
-		theMovieDb.search.getPerson({"query":"Brad%20Pitt"}, successCB, errorCB);
-
-
 		$("#resultPanel").show();
+		var input = $("#userInput").val();
+		var taco = theMovieDb.search.getPerson({"query":"Brad%20Pitt"}, successCB, errorCB);
 	});
-})
+});
+
+
+
+
+
+
+
+
 
